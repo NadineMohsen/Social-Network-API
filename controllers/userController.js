7e065d971d4cb1e749eecab06291c24ff1c70066
Nadine <nadine.mohsen@hotmail.com>
2022-05-03 18:12:30 +0100

    get single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
-const {User}= require('../models');
+const { createSecureServer } = require('http2');
+const {User, Thought}= require('../models');
 
 module.exports={
     //GET all users
@@ -8,15 +9,20 @@ module.exports={
         .catch((err)=> res.status(500).json(err))
     },
     //GET a single user by its _id and populated thought and friend data
-    // getSingleUser(req,res){
-    //     User.findOne({_id: req.params.userId})
-    //     .select('-__v')
-    //     .then((user)=>
-    //     !user
-    //         ? res.status(404).json({message: 'No user with that ID'})
-    //         : res.json(user)
-    //     )  
-    //     .catch((err)=> res.status(500).json(err))  
-    // }
+    getSingleUser(req,res){
+        User.findOne({_id: req.params.userId})
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends')
+        .then((user)=>
+        !user
+            ? res.status(404).json({message: 'No user with that ID'})
+            : res.json(user)
+        )  
+        .catch((err)=> res.status(500).json(err))  
+    },
     //POST a new user
+    // createUser(req,res){
+    //     User.create(req,)
+    // }
 }
