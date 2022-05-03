03a115626e59f488396a57b723c9fa2edcaea5d5
Nadine <nadine.mohsen@hotmail.com>
2022-05-03 17:21:24 +0100

    user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateSingleUser,
+    deleteSingleUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).delete(deleteSingleUser).put(updateSingleUser);
+
+module.exports = router;
